1030b9dbbfa6782c9828d949c67f4f235e59cbfe
JorgeLeao <jorge.leao@ufrj.br>
2022-10-26 23:35:28 -0300

    Finished basic Cataloging page also.

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -14,6 +14,10 @@ import './i18n.js';
 
 export default function Mainpage() {
   //console.log("=== React version: "+version);
+
+
+
+//=========== Search ==========
   
   const [chkboxState, setChkboxState] = useState(new Array(11).fill(false,0,11))
 
@@ -21,6 +25,7 @@ export default function Mainpage() {
     let newChkboxState = new Array(11).fill(false,0,11)
     setChkboxState(newChkboxState)
   }
+  // Checkboxes are numbered from 0 to 10 - 0:serial, 1:titleAND, 2:titleOR, ans so on...
   function serialChange(){
     let newChkboxState = [...chkboxState]  // to ensure it is a deep copy!
     newChkboxState[0] = !chkboxState[0]
@@ -90,12 +95,13 @@ export default function Mainpage() {
       case 'nextPage'  :{ console.log("=== NEXT PAGE ===");
                           setCurrPage(currPage => currPage+Number(1))
                           break;}
-      case 'search'    :{ let selectedChkboxes = ''
+      case 'search'    :{ console.log("=== SEARCH ===");
+                          let selectedChkboxes = ''
                           if(chkboxState[0])selectedChkboxes='serial'
                           else for(let i=1;i<11;i++){
                             if (chkboxState[i]) selectedChkboxes += chkboxNames[i] + '\n'
                           }
-                          setFields({serial:Number(1),title:"",author:"",publisher:"",from:"",to:"",keywords:""});
+                          setFields(fields => ({...fields}));
                           setResults(
                             '=== Fields:\n'+
                             fields.serial+'\n'+
@@ -128,7 +134,62 @@ export default function Mainpage() {
     setCurrPage(Number(e.target.value))
   }
 
-  const [nroRecords,setNroRecords]= useState(0)
+  const [nroRecords,setNroRecords] = useState(0)
+
+
+//=========== Cataloging ==========
+
+const [catalogFields, setCatalogFields] = useState({serial:"serial",title:"title",author:"author",publisher:"publisher",pubdate:"2022",keywords:"#React #Web",originalFilename:"BigFile.bin"})
+const [newComment,setNewComment] = useState("New comment ...")
+const [previousComment,setPreviousComment] = useState("Previous comments ...")
+
+function handleCatalogButtons(button){
+  switch(button){
+    case 'prevSerial':{ console.log("=== PREVIOUS SERIAL: ");
+                        break;}
+    case 'nextSerial':{ console.log("=== NEXT SERIAL: ");
+                        break;}
+    case 'upload':{ console.log("=== UPLOAD: ");
+                        break;}
+    case 'edit':{ console.log("=== EDIT: ");
+                    break;}
+    case 'saveascurrent':{ console.log("=== saveascurrent: ");
+                    break;}
+    case 'saveasnew':{ console.log("=== saveasnew: ");
+                    break;}
+    case 'download':{ console.log("=== download: ");
+                    break;}
+    case 'clear':{ console.log("=== clear: ");
+                    let updatedCatalogFields = {serial:"",title:"",author:"",publisher:"",pubdate:"",keywords:"",originalFilename:""};
+                    setCatalogFields(updatedCatalogFields);
+                    setNewComment(()=>(""))
+                    setPreviousComment(()=>(""))
+                    break;}
+    case 'delete':{ console.log("=== delete: ");
+                    break;}
+    default:{}
+  }
+}
+
+function catalogHandleChangeReducer(field,value){
+  console.log("=== value: "+value)
+  let updatedField = {}
+  switch(field){
+    case 'serial':{ updatedField = {serial:value}; console.log("=== SERIAL ===");break;}  
+    case 'title':{ updatedField = {title:value}; break;}  
+    case 'author':{ updatedField = {author:value}; break;}  
+    case 'publisher':{ updatedField = {publisher:value}; break;}  
+    case 'pubdate':{ updatedField = {pubdate:value}; break;}  
+    case 'keywords':{ updatedField = {keywords:value}; break;}  
+    case 'originalFilename':{ updatedField = {originalFilename:value}; break;}  
+    default:{}
+  }  
+  setCatalogFields(catalogFields => ({...catalogFields,...updatedField}))
+}
+
+function catalogHandleNewComment(e){
+  setNewComment(()=>(e.target.value))    
+}
 
   return (
     <div> 
@@ -145,7 +206,15 @@ export default function Mainpage() {
                                               nroRecords={nroRecords}
                                               textareavalue={results}/>}>
             </Route>
-            <Route path="/catalog" element={<Cataloging />}></Route>
+            <Route path="/catalog" element={<Cataloging
+                                              handleCatalogButtons={handleCatalogButtons}
+                                              catalogHandleChangeReducer={catalogHandleChangeReducer}
+                                              catalogFields={catalogFields}
+                                              newComment={newComment}
+                                              previousComment={previousComment}
+                                              catalogHandleNewComment={catalogHandleNewComment}
+                                              />}>
+            </Route>
             <Route path="/about" element={<About />}></Route>
             <Route path="/logout" element={<Logout />}></Route>
           </Routes>
--- src/cataloging.js
+++ src/cataloging.js
@@ -16,61 +16,77 @@ export default function Cataloging(props) {
                     <div className="cataloging_item1"></div>
 
                     <div className="cataloging_item2 cataloging_two-upper-buttons">
-                        <div className="cataloging_anterior">{t('cataloging.previous')}</div>
-                        <div className="cataloging_proximo">{t('cataloging.next')}</div>
+                        <div className="cataloging_anterior" onClick={()=>props.handleCatalogButtons('prevSerial')}>{t('cataloging.previous')}</div>
+                        <div className="cataloging_proximo" onClick={()=>props.handleCatalogButtons('nextSerial')}>{t('cataloging.next')}</div>
                     </div>
                     <div className="cataloging_item3"></div>
 
                     <div className="cataloging_item1">{t('cataloging.serial')}</div>
-                    <div className="cataloging_item2"><input type="text" className="cataloging_largeinput" /></div>
+                    <div className="cataloging_item2"><input type="text" className="cataloging_largeinput"
+                                                             onChange={e=>props.catalogHandleChangeReducer('serial', e.target.value)}
+                                                             value={props.catalogFields.serial}/></div>
                     <div className="cataloging_item3"></div>
 
                     <div className="cataloging_item1">{t('cataloging.title')}</div>
-                    <div className="cataloging_item2"><input type="text" className="cataloging_largeinput" /></div>
+                    <div className="cataloging_item2"><input type="text" className="cataloging_largeinput"
+                                                             onChange={e=>props.catalogHandleChangeReducer('title', e.target.value)}
+                                                             value={props.catalogFields.title}/></div>
                     <div className="cataloging_item3"></div>
 
                     <div className="cataloging_item1">{t('cataloging.author')}</div>
-                    <div className="cataloging_item2"><input type="text" className="cataloging_largeinput" /></div>
+                    <div className="cataloging_item2"><input type="text" className="cataloging_largeinput"
+                                                             onChange={e=>props.catalogHandleChangeReducer('author', e.target.value)}
+                                                             value={props.catalogFields.author}/></div>
                     <div className="cataloging_item3"></div>
 
                     <div className="cataloging_item1">{t('cataloging.publisher')}</div>
-                    <div className="cataloging_item2"><input type="text" className="cataloging_largeinput" /></div>
+                    <div className="cataloging_item2"><input type="text" className="cataloging_largeinput" 
+                                                             onChange={e=>props.catalogHandleChangeReducer('publisher', e.target.value)}
+                                                             value={props.catalogFields.publisher}/></div>
                     <div className="cataloging_item3"></div>
 
                     <div className="cataloging_item1">{t('cataloging.pubdate')}</div>
-                    <div className="cataloging_item2"><input type="text" className="cataloging_largeinput" /></div>
+                    <div className="cataloging_item2"><input type="text" className="cataloging_largeinput"
+                                                             onChange={e=>props.catalogHandleChangeReducer('pubdate', e.target.value)}
+                                                             value={props.catalogFields.pubdate}/></div>
                     <div className="cataloging_item3"></div>
 
                     <div className="cataloging_item1">{t('cataloging.keywords')}</div>
-                    <div className="cataloging_item2"><input type="text" className="cataloging_largeinput" /></div>
+                    <div className="cataloging_item2"><input type="text" className="cataloging_largeinput"
+                                                             onChange={e=>props.catalogHandleChangeReducer('keywords', e.target.value)}
+                                                             value={props.catalogFields.keywords}/></div>
                     <div className="cataloging_item3"></div>
 
                     <div className="cataloging_item1">{t('cataloging.originalfilename')}</div>
-                    <div className="cataloging_item2"><input type="text" className="cataloging_largeinput" /></div>
+                    <div className="cataloging_item2"><input type="text" className="cataloging_largeinput"
+                                                             onChange={e=>props.catalogHandleChangeReducer('originalFilename', e.target.value)}
+                                                             value={props.catalogFields.originalFilename}/></div>
                     <div className="cataloging_item3"></div>
 
                     <div className="cataloging_item1">{t('cataloging.fileupload')}</div>
                     <div className="cataloging_item2"><input type="file" className="cataloging_largeinput_file" /></div>
-                    <div className="cataloging_item3"><div className="cataloging_upload">{t('cataloging.upload')}</div></div>
+                    <div className="cataloging_item3"><div className="cataloging_upload" onClick={()=>props.handleCatalogButtons('upload')}>{t('cataloging.upload')}</div></div>
 
                     <div className="cataloging_itemC">{t('cataloging.newcomment')}</div>
-                    <div className="cataloging_item2"><textarea className="cataloging_newcomment" /></div>
+                    <div className="cataloging_item2"><textarea className="cataloging_newcomment"
+                                                                value={props.newComment}
+                                                                onChange={e=>props.catalogHandleNewComment(e)}/></div>
                     <div className="cataloging_item3"></div>
 
                     <div className="cataloging_itemC">{t('cataloging.previouscomments')}</div>
-                    <div className="cataloging_item2"><textarea className="cataloging_newcomment" /></div>
+                    <div className="cataloging_item2"><textarea className="cataloging_newcomment" value={props.previousComment} readOnly /></div>
                     <div className="cataloging_item3"></div>
 
                     <div className="cataloging_item1"></div>
-                    <div className="cataloging_item4"><div className="cataloging_botbutton">{t('cataloging.edit')}</div></div>
-                    <div className="cataloging_item4"><div className="cataloging_botbutton">{t('cataloging.saveascurrent')}</div></div>
-                    <div className="cataloging_item4"><div className="cataloging_botbutton">{t('cataloging.saveasnew')}</div></div>
+                    <div className="cataloging_item4"><div className="cataloging_botbutton" onClick={()=>props.handleCatalogButtons('edit')}>{t('cataloging.edit')}</div></div>
+                    <div className="cataloging_item4"><div className="cataloging_botbutton" onClick={()=>props.handleCatalogButtons('saveascurrent')}>{t('cataloging.saveascurrent')}</div></div>
+                    <div className="cataloging_item4"><div className="cataloging_botbutton" onClick={()=>props.handleCatalogButtons('saveasnew')}>{t('cataloging.saveasnew')}</div></div>
                     <div className="cataloging_item3"></div>
 
                     <div className="cataloging_item1"></div>
-                    <div className="cataloging_item4"><div className="cataloging_botbutton">{t('cataloging.download')}</div></div>
-                    <div className="cataloging_item4"><div className="cataloging_botbutton">{t('cataloging.clear')}</div></div>
-                    <div className="cataloging_item4"><div className="cataloging_botbutton">{t('cataloging.delete')}</div></div>
+                    <div className="cataloging_item4"><div className="cataloging_botbutton" onClick={()=>props.handleCatalogButtons('download')}>{t('cataloging.download')}</div></div>
+                    <div className="cataloging_item4"><div className="cataloging_botbutton" onClick={()=>props.handleCatalogButtons('clear')}>{t('cataloging.clear')}</div></div>
+                    <div className="cataloging_item4"><div className="cataloging_botbutton" onClick={()=>props.handleCatalogButtons('delete')}>{t('cataloging.delete')}</div></div>
                     <div className="cataloging_item3"></div>
                 </div>
             </form>                        
