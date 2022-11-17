e6b8a2abbb1e594dfdf0704f30f96af29c8ec08a
JorgeLeao <jorge.leao@ufrj.br>
2022-11-17 16:49:17 -0300

    Small correction

--- src/logout.js
+++ src/logout.js
@@ -1,12 +1,9 @@
 import React from "react";
 import './logout.css'
-import Messager2 from './messager2/messager'
-import  { useMessager }  from './messager2/useMessager.js'
 
 
 
 export default function Logout(props) {
-    const { isActive, message, progress, openMessager } = useMessager();
 
 
     return (
@@ -14,9 +11,6 @@ export default function Logout(props) {
             <br />
             <br />
             <br/>
-            <Messager2 isActive = {isActive} message={message} progress={progress}/>
-
-            <button onClick={() => openMessager('...Messager 2...')} >Open Messager2</button>
         </div>
     );
 }
